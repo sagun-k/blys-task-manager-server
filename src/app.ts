eaa931ad785beefdb8328b72b0673ca176acd0e1
sagun-k <sagunkshrestha7@gmail.com>
2025-07-25 14:35:19 +0545

    removed dual listen been called inside app.ts

--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 app.use(cors({
     origin: true,
     credentials: true,
